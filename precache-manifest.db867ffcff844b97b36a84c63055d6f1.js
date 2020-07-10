a48de8da1baae41297f35dc210866c1b19b33cd8
Yubo Diwu <yubodiwu@MacBook-Pro-3.local>
2020-07-10 11:13:48 -0700

    Updates

---
+++ precache-manifest.db867ffcff844b97b36a84c63055d6f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40b1d3c888a258a332fc372134d57729",
+    "url": "/gp-vjs-pages/index.html"
+  },
+  {
+    "revision": "777ddabb8cadec12db1b",
+    "url": "/gp-vjs-pages/static/css/2.22fc193c.chunk.css"
+  },
+  {
+    "revision": "a844163f4d7e571de32a",
+    "url": "/gp-vjs-pages/static/css/main.d0ee7a36.chunk.css"
+  },
+  {
+    "revision": "777ddabb8cadec12db1b",
+    "url": "/gp-vjs-pages/static/js/2.3f291ac9.chunk.js"
+  },
+  {
+    "revision": "a17dd6d57e33dda6b1c2927e8d6cf97e",
+    "url": "/gp-vjs-pages/static/js/2.3f291ac9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a844163f4d7e571de32a",
+    "url": "/gp-vjs-pages/static/js/main.e72d16a8.chunk.js"
+  },
+  {
+    "revision": "206b138209bed3f7bf01",
+    "url": "/gp-vjs-pages/static/js/runtime-main.56b0f95f.js"
+  }
+]);
